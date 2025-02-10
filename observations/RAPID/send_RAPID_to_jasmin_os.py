dcb3e07158cfa9f7802ba790924cf36172cb69c6
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-02-10 17:07:30 +0000

    Added scripts to download, post-process and send MOVE, SAMBA & RAPID data to JASMIN OS.

---
+++ observations/RAPID/send_RAPID_to_jasmin_os.py
@@ -0,0 +1,54 @@
+"""
+send_RAPID_to_jasmin_os.py
+
+Description: Python script to send the RAPID-MOCHA
+overturning  and transports to the JASMIN Object Store.
+
+Python Environment: env_jasmin_os [conda]
+
+Created by: Ollie Tooth (oliver.tooth@noc.ac.uk)
+Created on: 2024-12-19
+"""
+# -- Import Python packages -- #
+import sys
+import glob
+import xarray as xr
+from tqdm import tqdm
+
+# Add path to msm-os package:
+sys.path.append('/dssgfs01/working/otooth/AtlantiS/jasmin_os/msm-os/src/')
+from msm_os.object_store import ObjectStoreS3
+
+# -- Define JASMIN S3 object store -- #
+store_credentials_json = '/dssgfs01/working/otooth/AtlantiS/jasmin_os/credentials/jasmin_os_credentials.json'
+obj_store = ObjectStoreS3(anon=False, store_credentials_json=store_credentials_json)
+print('Completed: Initialised JASMIN S3 object store using user credentials.')
+
+# -- Send RAPID Observations to JASMIN Object Store -- #
+print('In Progress: Sending RAPID Observations to JASMIN Object Store...')
+
+# Define path to RAPID obs.
+ini_fpath = '/dssgfs01/scratch/otooth/npd_data/observations/RAPID/data/*.nc'
+files = glob.glob(ini_fpath)
+
+# Define bucket name:
+bucket = 'npd-obs'
+
+# Iterate over observation files:
+for file in files:
+    # Define path to destination zarr store:
+    filename = file.split('/')[-1]
+    dest = f"{bucket}/RAPID/{filename.replace('.nc', '')}"
+    print(f'--> Sending: {dest} ...')
+
+    # -- Write climatology to .zarr store -- #
+    # Define s3fs mapper to destination:
+    mapper = obj_store.get_mapper(dest)
+
+    # -- Import & Processing: Add metadata attributes -- #
+    ds = xr.open_dataset(file)
+
+    # -- Write to JASMIN OS as .zarr store -- #
+    ds.to_zarr(mapper, mode='w', consolidated=True)
+
+print('Completed: Sent RAPID Observations to JASMIN Object Store.')
