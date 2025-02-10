c2f00782341a870b2d24c80e8420c24d16918d5b
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-02-10 17:06:21 +0000

    Added scripts to download, post-process and send OISSTv2 data to JASMIN OS.

---
+++ observations/OISST/send_OISSTv2_to_jasmin_os.py
@@ -0,0 +1,79 @@
+"""
+send_OISSTv2_to_jasmin_os.py
+
+Description: Python script to send the OISSTv2 hydrographic
+section climatology (1990-2012) data to the JASMIN Object Store.
+
+Python Environment: env_jasmin_os [conda]
+
+Created by: Ollie Tooth (oliver.tooth@noc.ac.uk)
+Created on: 2024-12-19
+"""
+# -- Import Python packages -- #
+import sys
+import glob
+import numpy as np
+import xarray as xr
+
+# Add path to msm-os package:
+sys.path.append('/dssgfs01/working/otooth/AtlantiS/jasmin_os/msm-os/src/')
+from msm_os.object_store import ObjectStoreS3
+
+# -- Define JASMIN S3 object store -- #
+store_credentials_json = '/dssgfs01/working/otooth/AtlantiS/jasmin_os/credentials/jasmin_os_credentials.json'
+obj_store = ObjectStoreS3(anon=False, store_credentials_json=store_credentials_json)
+print('Completed: Initialised JASMIN S3 object store using user credentials.')
+
+# -- Send OISSTv2 Sea Ice Observations to JASMIN Object Store -- #
+print('In Progress: Sending OISSTv2 SI Observations to JASMIN Object Store...')
+
+# Define path to OISSTv2 obs.
+ini_fpath = "http://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2.highres/icec.mon.ltm.1991-2020.nc"
+
+# Define bucket name:
+bucket = 'npd-obs'
+# Define path to destination zarr store:
+dest = f"{bucket}/OISSTv2/OISSTv2_icec_global_monthly_climatology_1991_2020"
+
+# -- Write climatology to .zarr store -- #
+# Define s3fs mapper to destination:
+mapper = obj_store.get_mapper(dest)
+
+# -- Import & Processing: Add metadata attributes -- #
+ds = xr.open_dataset(ini_fpath, decode_times=False)
+ds = ds.rename({'time': 'month'})
+ds = ds.assign_coords(month=np.arange(1, 13))
+
+# Add description attribute:
+ds.attrs['description'] = 'Long-term mean 1991-2020 sea ice concentration from Optimally Interpolated Sea Surface Temperature (OISST) dataset version 2. Downloaded from http://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2.highres/icec.mon.ltm.1991-2020.nc on 19/12/2024. Transferred to JASMIN Object Store on 19/12/2024.'
+
+# -- Write to JASMIN OS as .zarr store -- #
+ds.to_zarr(mapper, mode='w', consolidated=True)
+
+print('Completed: Sent OISSTv2 SI Observations to JASMIN Object Store.')
+
+# -- Send OISSTv2 Sea Surface Temperature Observations to JASMIN Object Store -- #
+print('In Progress: Sending OISSTv2 SST Observations to JASMIN Object Store...')
+
+# Define path to OISSTv2 obs.
+ini_fpath = "http://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2.highres/sst.mon.ltm.1991-2020.nc"
+
+# Define path to destination zarr store:
+dest = f"{bucket}/OISSTv2/OISSTv2_sst_global_monthly_climatology_1991_2020"
+
+# -- Write climatology to .zarr store -- #
+# Define s3fs mapper to destination:
+mapper = obj_store.get_mapper(dest)
+
+# -- Import & Processing: Add metadata attributes -- #
+ds = xr.open_dataset(ini_fpath, decode_times=False)
+ds = ds.rename({'time': 'month'})
+ds = ds.assign_coords(month=np.arange(1, 13))
+
+# Add description attribute:
+ds.attrs['description'] = 'Long-term mean 1991-2020 sea surface temperature from Optimally Interpolated Sea Surface Temperature (OISST) dataset version 2. Downloaded from http://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2.highres/icec.mon.ltm.1991-2020.nc on 19/12/2024. Transferred to JASMIN Object Store on 19/12/2024.'
+
+# -- Write to JASMIN OS as .zarr store -- #
+ds.to_zarr(mapper, mode='w', consolidated=True)
+
+print('Completed: Sent OISSTv2 SST Observations to JASMIN Object Store.')
