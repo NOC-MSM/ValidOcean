ff62fc24a2e8194f1a67fe9940ea0ba9a6c8e3f1
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-02-21 17:57:50 +0000

    Added .py script to download monthly RP GlobCurrent data from Copernicus.

---
+++ observations/GlobCurrent/download_GlobCurrent_monthly_RP_1993_2023.py
@@ -0,0 +1,33 @@
+#########################################################################
+"""
+Download Global Current dataset consisting of Ekman and
+Geostrophic currents at the Surface and 15m from Copernicus Marine.
+This script downloads the monthly reprocessed files from 1993 to 2022.
+
+Created By: Ollie Tooth
+Created On: 2025-02-21
+Contact: oliver.tooth@noc.ac.uk
+
+Note: This download script should be run using the env_npd_obs environment.
+"""
+#########################################################################
+
+# Import the Copernicus Marine API toolbox:
+import copernicusmarine
+
+# Define filepath to credentials:
+credentials_fpath = "/home/otooth/.copernicusmarine/.copernicusmarine-credentials"
+# Define output directory:
+out_fdir = "/dssgfs01/scratch/otooth/npd_data/observations/GlobCurrent/data/"
+
+# Download the GlobCurrent analysis dataset:
+for year in range(1995, 2024):
+    copernicusmarine.subset(
+      dataset_id="cmems_obs-mob_glo_phy-cur_my_0.25deg_P1M-m",
+      start_datetime=f"{year}-01-01T00:00:00",
+      end_datetime=f"{year}-12-01T00:00:00",
+      credentials_file=credentials_fpath,
+      output_directory=out_fdir,
+      file_format="zarr",
+      output_filename=f"globcurrent_my_0.25deg_P1M-m_{year}.zarr",
+    )
