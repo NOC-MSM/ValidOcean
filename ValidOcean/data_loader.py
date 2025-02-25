4b0f43ff70f24b6685abf00040b5e694520ff550
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-02-25 18:52:10 +0000

    Updated DataLoaders to use external pre-processing funcs for subsetting and climatology calculations.

--- ValidOcean/data_loader.py
+++ ValidOcean/data_loader.py
@@ -13,78 +13,9 @@ import abc
 import numpy as np
 import xarray as xr
 
-# -- Utility Functions -- #
-def _transform_longitudes(data: xr.DataArray) -> xr.DataArray:
-    """
-    Transform longitudes in a given xarray DataArray
-    to the range [-180, 180].
+from ValidOcean.preprocess import _subset_data, _compute_climatology, _transform_longitudes
 
-    Parameters
-    ----------
-    data : xarray.DataArray
-        DataArray containing longitudes variable, ``lon``.
-
-    Returns
-    -------
-    xarray.DataArray
-        DataArray containing longitudes transformed to
-        the range [-180, 180].
-    """
-    # -- Verify Inputs -- #
-    if not isinstance(data, xr.DataArray):
-        raise TypeError("``data`` must be an xarray DataArray.")
-    if 'lon' not in data.dims:
-        raise ValueError("``data`` must contain variable ``lon``.")
-    
-    # -- Transform Longitudes -- #:
-    data['lon'] = xr.where(data['lon'] > 180, data['lon'] - 360, data['lon'])
-    data = data.sortby('lon', ascending=True)
-
-    return data
-
-def _compute_climatology(data: xr.DataArray, freq: str = 'total') -> xr.DataArray:
-    """
-    Compute the climatology of a given xarray DataArray
-    at the specified frequency.
-
-    Parameters
-    ----------
-    data : xarray.DataArray
-        DataArray containing the variable to compute the
-        climatology of.
-    freq : str, default: ``total``
-        Climatology frequency to compute. Options include
-        ``total``, ``seasonal``, ``monthly``.
-
-    Returns
-    -------
-    xarray.DataArray
-        DataArray containing the climatology of the input
-        data at the specified frequency.
-    """
-    # -- Verify Inputs -- #
-    if not isinstance(data, xr.DataArray):
-        raise TypeError("``data`` must be an xarray DataArray.")
-    if 'time' not in data.dims:
-        raise ValueError("``data`` must contain variable ``time``.")
-    if not np.issubdtype(data.time.dtype, np.datetime64):
-        raise TypeError("variable ``time`` dtype must be datetime64.")
-    if not isinstance(freq, str):
-        raise TypeError("``freq`` must be a string.")
-    if freq not in ['total', 'seasonal', 'monthly']:
-        raise ValueError("``freq`` must be one of 'total', 'seasonal', 'monthly'.")
-
-    # -- Compute Climatology -- #
-    if freq == 'total':
-        data = data.mean(dim='time')
-    elif freq == 'seasonal':
-        data = data.groupby('time.season').mean()
-    elif freq == 'monthly':
-        data = data.groupby('time.month').mean()
-
-    return data
-
-# -- Define DataLoader Class -- #
+# -- DataLoader Abstract Base Class -- #
 class DataLoader(abc.ABC):
     """
     DataLoader Base Class to load ocean observations
@@ -101,12 +32,17 @@ class DataLoader(abc.ABC):
         observations stored in a local filesystem or a url path
         for ocean observations stored by an alternative cloud
         provider.
-    region : str, default: ``global``
+    region : str, default: ``None``
         Region of ocean observations dataset to load.
-        Options include ``global``, ``arctic``, ``antarctic``.
-    freq : str, default: ``total``
+        Options include ``None``, ``arctic``, ``antarctic``.
+    bounds : slice, default: None
+        Time bounds to compute climatology using ocean observations.
+        Default is None, meaning the entire ocean observations dataset
+        is used.
+    freq : str, default: ``None``
         Climatology frequency of the ocean observations dataset.
-        Options include ``total``, ``seasonal``, ``monthly``.
+        Options include ``None``, ``total``, ``seasonal``, ``monthly``.
+        ``None`` means no climatology is computed from available data.
 
     Attributes
     ----------
@@ -116,6 +52,8 @@ class DataLoader(abc.ABC):
         Source of ocean observations data.
     _region : str
         Region of ocean observations dataset to load.
+    _bounds : slice
+        Time bounds to compute climatology using ocean observations.
     _freq : str
         Climatology frequency of the ocean observations dataset.
     """
@@ -123,7 +61,8 @@ class DataLoader(abc.ABC):
                  var_name: str,
                  source: str = 'jasmin-os',
                  region: str | None = None,
-                 freq: str = 'total'
+                 bounds: slice | None = None,
+                 freq: str | None = None,
                  ):
         # -- Verify Inputs -- #
         if not isinstance(var_name, str):
@@ -133,11 +72,22 @@ class DataLoader(abc.ABC):
         if region is not None:
             if not isinstance(region, str):
                 raise TypeError("``region`` must be a specfied as a string.")
-        if not isinstance(freq, str):
-            raise TypeError("``freq`` must be a specfied as a string.")
+        if bounds is not None:
+            if not isinstance(bounds, slice):
+                raise TypeError("``bounds`` must be a specfied as a slice.")
+            if not isinstance(bounds.start, str):
+                raise TypeError("``bounds.start`` must be specified as a datetime string (e.g., 'YYYY-MM').")
+            if not isinstance(bounds.stop, str):
+                raise TypeError("``bounds.stop`` must be specified as a datetime string (e.g., 'YYYY-MM')")
+        if freq is not None:
+            if not isinstance(freq, str):
+                raise TypeError("``freq`` must be a specfied as a string.")
+            if freq not in ['total', 'seasonal', 'monthly']:
+                raise ValueError("``freq`` must be one of 'total', 'seasonal', 'monthly'.")
         
         # -- Define DataLoader Attributes -- #
         self._var_name = var_name
+        self._bounds = bounds
         self._freq = freq
         self._region = region
         if source == 'jasmin-os':
@@ -169,18 +119,30 @@ class OISSTv2Loader(DataLoader):
     ----------
     var_name : str, default: ``sst``
         Name of variable to load from OISSTv2 ocean observations.
-    freq : str, default: ``total``
+        Options include ``sst``, ``siconc``.
+    bounds : slice, default: None
+        Time bounds to compute climatology using OISSTv2 ocean observations.
+        Default is None, meaning the entire dataset is considered.
+    freq : str, default: ``None``
         Climatology frequency of the OISSTv2 ocean observations.
-        Options include ``total``, ``seasonal``, ``monthly``.
+        Options include ``None``, ``total``, ``seasonal``, ``monthly``.
+        ``None`` means no climatology is computed from monthly data.
     """
     def __init__(self,
                  var_name: str = 'sst',
-                 freq: str = 'total'
+                 region: str | None = None,
+                 bounds: slice | None = None,
+                 freq: str | None = None,
                  ):
+        # -- Verify Inputs -- #
+        if var_name not in ['sst', 'siconc']:
+            raise ValueError("``var_name`` must be one of 'sst', 'siconc'.")
 
         # -- Initialise DataLoader -- #
         super().__init__(var_name=var_name,
+                         region=region,
                          source='jasmin-os',
+                         bounds=bounds,
                          freq=freq)
 
     def _load_data(self) -> xr.DataArray:
@@ -195,96 +157,22 @@ class OISSTv2Loader(DataLoader):
             climatology at specified frequency.
         """
         # Load data from the JASMIN Object Store:
-        url = f"{self._source}/OISSTv2/OISSTv2_sst_global_monthly_climatology_1991_2020/"
+        if self._var_name == 'sst':
+            url = f"{self._source}/OISSTv2/OISSTv2_sst_global_monthly_1981_2025/"
+        elif self._var_name == 'siconc':
+            url = f"{self._source}/OISSTv2/OISSTv2_siconc_global_monthly_1981_2025/"
         data = xr.open_zarr(url, consolidated=True)[self._var_name]
-
-        # Transform time to datetime64:
-        data = data.rename({'month': 'time'})
-        dates = np.datetime64('2001-01', 'M') + (np.timedelta64(1, 'M') * np.arange(data['time'].size))
-        data['time'] = xr.DataArray(dates.astype('datetime64[ns]'), dims='time')
     
         # Transform longitudes to [-180, 180]:
         data = _transform_longitudes(data)
 
+        # Extract climatological period:
+        if self._bounds is not None:
+            data = _subset_data(data, bounds=self._bounds)
+
         # Compute climatology:
-        data = _compute_climatology(data, freq=self._freq)
-
-        # Add climatology time-window attributes:
-        data.attrs['start_time'] = '1991-01-01'
-        data.attrs['end_time'] = '2020-12-31'
-
-        return data
-    
-# -- CCI DataLoader -- #
-class CCILoader(DataLoader):
-    """
-    DataLoader to load CCI sea surface temperature or
-    sea ice fraction observations stored in the JASMIN 
-    Object Store.
-
-    Parameters
-    ----------
-    var_name : str
-        Name of variable to load from CCI ocean observations.
-        Options include ``sst``, ``siconc``.
-    freq : str, default: ``total``
-        Climatology frequency of the CCI ocean observations.
-        Options include ``total``, ``seasonal``, ``monthly``.
-    """
-    def __init__(self,
-                 var_name: str,
-                 freq: str = 'total'
-                 ):
-        # -- Verify Inputs -- #
-        if not isinstance(var_name, str):
-            raise TypeError("``var_name`` must be a specfied as a string.")
-        if var_name not in ['sst', 'siconc']:
-            raise ValueError("``var_name`` must be one of 'sst', 'siconc'.")
-        if not isinstance(freq, str):
-            raise TypeError("``freq`` must be a specfied as a string.")
-        if freq not in ['total', 'seasonal', 'monthly']:
-            raise ValueError("``freq`` must be one of 'total', 'seasonal', 'monthly'.")
-
-        # -- Initialise DataLoader -- #
-        if var_name == 'sst':
-            super().__init__(var_name='analysed_sst',
-                             source='jasmin-os',
-                             freq=freq)
-        elif var_name == 'siconc':
-            super().__init__(var_name='sea_ice_fraction',
-                             source='jasmin-os',
-                             freq=freq)
-
-    def _load_data(self) -> xr.DataArray:
-        """
-        Load CCI variable climatology from observations
-        stored in the JASMIN Object Store.
-
-        Returns
-        -------
-        xarray.DataArray
-            Dataset storing CCI variable
-            climatology at specified frequency.
-        """
-        # Load data from the JASMIN Object Store:
-        if self._freq == "total":
-            url = f"{self._source}/CCI/ESACCI-v3.0-SST_global_climatology_1991_2020/"
-        elif self._freq == "seasonal":
-            url = f"{self._source}/CCI/ESACCI-v3.0-SST_global_seasonal_climatology_1991_2020/"
-        elif self._freq == "monthly":
-            url = f"{self._source}/CCI/ESACCI-v3.0-SST_global_monthly_climatology_1991_2020/"
-
-        data = xr.open_zarr(url, consolidated=True)[self._var_name]
-
-        # Standardise variable name:
-        if self._var_name == 'analysed_sst':
-            data = data.rename({'sst'})
-        elif self._var_name == 'sea_ice_fraction':
-            data = data.rename({'siconc'})
-
-        # Add climatology time-window attributes:
-        data.attrs['start_time'] = '1991-01-01'
-        data.attrs['end_time'] = '2020-12-31'
+        if self._freq is not None:
+            data = _compute_climatology(data, freq=self._freq)
 
         return data
 
@@ -303,13 +191,18 @@ class NSIDCLoader(DataLoader):
     region : str, default: ``arctic``
         Region of NSIDC observations dataset to load.
         Options include ``arctic``, ``antarctic``.
+    bounds : slice, default: None
+        Time bounds to compute climatology using NSIDC ocean observations.
+        Default is None, meaning the entire dataset is considered.
     freq : str, default: ``total``
-        Climatology frequency of the CCI ocean observations.
-        Options include ``total``, ``seasonal``, ``monthly``.
+        Climatology frequency of the NSIDC ocean observations.
+        Options include ``None``, ``total``, ``seasonal``, ``monthly``.
+        ``None`` means no climatology is computed from monthly data.
     """
     def __init__(self,
                  var_name: str,
                  region: str = 'arctic',
+                 bounds: slice | None = None,
                  freq: str = 'total'
                  ):
         # -- Verify Inputs -- #
@@ -321,15 +214,12 @@ class NSIDCLoader(DataLoader):
             raise TypeError("``region`` must be a specfied as a string.")
         if region not in ['arctic', 'antarctic']:
             raise ValueError("``region`` must be one of 'arctic', 'antarctic'.")
-        if not isinstance(freq, str):
-            raise TypeError("``freq`` must be a specfied as a string.")
-        if freq not in ['total', 'seasonal', 'monthly']:
-            raise ValueError("``freq`` must be one of 'total', 'seasonal', 'monthly'.")
 
         # -- Initialise DataLoader -- #
         super().__init__(var_name=var_name,
                          source='jasmin-os',
                          region=region,
+                         bounds=bounds,
                          freq=freq)
 
     def _load_data(self) -> xr.DataArray:
@@ -351,8 +241,8 @@ class NSIDCLoader(DataLoader):
 
         data = xr.open_zarr(url, consolidated=True)[self._var_name]
 
-        # Add climatology time-window attributes:
-        data.attrs['start_time'] = '1991-01-01'
-        data.attrs['end_time'] = '2020-12-31'
+        # Compute climatology:
+        if self._freq is not None:
+            data = _compute_climatology(data, bounds=self._bounds, freq=self._freq)
 
         return data
