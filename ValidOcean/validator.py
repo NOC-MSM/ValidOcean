e202fa68a736deb8785c4e4c25ccc132b82b4392
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-02-25 18:56:55 +0000

    Updated ModelValidator to use climatology bounds and region arguments, added new load_observations() method.

--- ValidOcean/validator.py
+++ ValidOcean/validator.py
@@ -16,7 +16,7 @@ from typing import Self
 # -- Import utility functions -- #
 import ValidOcean.data_loader as data_loader
 from ValidOcean.data_loader import DataLoader
-from ValidOcean.preprocess import _preprocess_model_data
+from ValidOcean.preprocess import _subset_data, _compute_climatology
 from ValidOcean.statistics import _compute_agg_stats
 from ValidOcean.regridding import _regrid_data
 from ValidOcean.plotting import _plot_global_2d
@@ -70,6 +70,7 @@ class ModelValidator():
         # -- Class Attributes -- #
         self._data = mdl_data
         self._dataloader = dataloader
+        self._obs = xr.Dataset()
         self._results = xr.Dataset()
         self._stats = {}
 
@@ -81,14 +82,21 @@ class ModelValidator():
     def data(self) -> xr.Dataset:
         return self._data
     @data.setter
-    def results(self, value: xr.Dataset = xr) -> None:
+    def results(self, value: xr.Dataset) -> None:
         self._data = value
+
+    @property
+    def obs(self) -> xr.Dataset:
+        return self._obs
+    @obs.setter
+    def results(self, value: xr.Dataset) -> None:
+        self._obs = value
 
     @property
     def results(self) -> xr.Dataset:
         return self._results
     @results.setter
-    def results(self, value: xr.Dataset = xr) -> None:
+    def results(self, value: xr.Dataset) -> None:
         self._results = value
 
     @property
@@ -99,11 +107,13 @@ class ModelValidator():
         self._stats = value
 
     # -- Class Methods -- #
-    def load_observations(self,
-                          obs_name : str,
-                          var_name : str,
-                          freq : str = 'total'
-                          ) -> Self:
+    def _load_obs_data(self,
+                       obs_name : str,
+                       var_name : str,
+                       region : str | None = None,
+                       bounds : slice | None = None,
+                       freq : str | None = None,
+                       ) -> Self:
         """
         Load ocean observations dataset.
 
@@ -113,52 +123,52 @@ class ModelValidator():
             Name of observational dataset to load.
         var_name : str
             Name of variable to load from observational dataset.
-        freq : str, default: ``total``
+        region : str, default: ``None``
+            Region of ocean observations dataset to load. Default is ``None``
+            meaning the entire dataset is loaded.
+        bounds : slice, default: None
+            Time bounds to compute climatology using ocean observations.
+            Default is ``None`` meaning the entire time series is loaded.
+        freq : str, default: ``None``
             Climatology frequency of the observational dataset.
-            Options include ``total``, ``seasonal``, ``monthly``.
+            Options include ``None``, ``total``, ``seasonal``, ``monthly``.
+            Default is ``None`` meaning the entire time series is loaded.
         
         Returns
         -------
         ModelValidator
-            ModelValidator object including observational data stored in ``results``
+            ModelValidator object including observational data stored in ``obs``
             attribute.
         """
         # -- Verify Inputs -- #
         if not isinstance(obs_name, str):
             raise TypeError("``obs_name`` must be specified as a string.")
-        if not isinstance(var_name, str):
-            raise TypeError("``var_name`` must be specified as a string.")
-        if not isinstance(freq, str):
-            raise TypeError("``freq`` must be specified as a string.")
-        if freq not in ['total', 'seasonal', 'monthly']:
-            raise ValueError("``freq`` must be one of ``total``, ``seasonal`` or ``monthly``.")
         
         # -- Load Ocean Observations Dataset -- #
         if self._dataloader is None:
             if hasattr(data_loader, f"{obs_name}Loader"):
                 ObsDataLoader = getattr(data_loader, f"{obs_name}Loader")
-                obs_data = ObsDataLoader(var_name=var_name, freq=freq)._load_data()
+                obs_data = ObsDataLoader(var_name=var_name, region=region, bounds=bounds, freq=freq)._load_data()
             else:
                 raise ValueError(f"undefined DataLoader specified: {obs_name}Loader.")
         else:
-            obs_data = self._dataloader(var_name=var_name, freq=freq)._load_data()
+            obs_data = self._dataloader(var_name=var_name, region=region, bounds=bounds, freq=freq)._load_data()
 
-        # -- Store Observational Data -- #
-        self.results[f"{var_name}_{obs_name}"] = obs_data.rename({'lon':f"lon_{obs_name}", 'lat':f"lat_{obs_name}"})
+        return obs_data
 
-        return self
 
     def _compute_2D_error(self,
                           var_name : str = 'tos_con',
                           obs : dict = dict(name='OISSTv2', var='sst'),
+                          bounds : slice | None = None,
                           freq : str = 'total',
                           regrid_to : str = 'model',
                           method : str = 'bilinear',
                           agg_stats : bool = True,
                           ) -> Self:
         """
-        Compute 2-dimensional variable error between ocean model output
-        and observations.
+        Compute error between ocean model output
+        and observations for 2-dimensional variable.
 
         Parameters
         ----------
@@ -167,6 +177,10 @@ class ModelValidator():
         obs : dict, default: {``name``:``OISSTv2``, ``var``: ``sst``}
             Dictionary defining the ``name`` of the observational
             dataset and variable ``var`` to calculate model error.
+        bounds : slice, default: ``None``
+            Time bounds to compute 2-dimensional variable climatology
+            using ocean model outputs and observations. Default is ``None``
+            meaning the entire ocean model time series is used.
         freq : str, default: ``total``
             Frequency to calculate ocean model error. 
             Options include ``total``, ``seasonal``, ``monthly``.
@@ -182,8 +196,8 @@ class ModelValidator():
         Returns
         -------
         ModelValidator
-            ModelValidator object including variable, {var_name}_error, stored in ``results``
-            attribute and aggregate statistics stored in ``stats`` attribute.
+            ModelValidator object including (ocean model - observation) error, stored in the
+            ``results`` attribute and aggregate statistics stored in the ``stats`` attribute.
         """
         # -- Verify Inputs -- #
         if not isinstance(var_name, str):
@@ -193,44 +207,25 @@ class ModelValidator():
         
         if not isinstance(obs, dict):
             raise TypeError("``obs`` must be specified as a dictionary.")
-        if 'name' not in obs.keys():
-            raise ValueError("``obs`` dictionary must contain key ``name``.")
-        if 'var' not in obs.keys():
-            raise ValueError("``obs`` dictionary must contain key ``var``.")
-
-        if not isinstance(freq, str):
-            raise TypeError("``freq`` must be specified as a string.")
-        if freq not in ['total', 'seasonal', 'monthly']:
-            raise ValueError("``freq`` must be one of ``total``, ``seasonal`` or ``monthly``.")
+        for key in ['name', 'region', 'var']:
+            if key not in obs.keys():
+                raise ValueError(f"``obs`` dictionary must contain key ``{key}``.")
 
         if not isinstance(regrid_to, str):
             raise TypeError("``regrid`` must be specified as a string.")
         if regrid_to not in ['model', 'obs']:
             raise ValueError("``regrid`` must be one of ``model`` or ``obs``.")
 
-        if not isinstance(method, str):
-            raise TypeError("``method`` must be specified as a string.")
-        if method not in ['bilinear', 'nearest', 'conservative']:
-            raise ValueError("``method`` must be one of ``bilinear``, ``nearest`` or ``conservative``.")
-
         if not isinstance(agg_stats, bool):
             raise TypeError("``agg_stats`` must be specified as a boolean.")
 
         # -- Load Observational Data -- #
-        if self._dataloader is None:
-            if hasattr(data_loader, f"{obs['name']}Loader"):
-                ObsDataLoader = getattr(data_loader, f"{obs['name']}Loader")
-                obs_data = ObsDataLoader(var_name=obs['var'], freq=freq)._load_data()
-            else:
-                raise ValueError(f"undefined DataLoader specified: {obs['name']}Loader.")
-        else:
-            obs_data = self._dataloader(var_name=obs['var'], freq=freq)._load_data()
+        obs_data = self._load_obs_data(obs_name=obs['name'], var_name=obs['var'], region=obs['region'], bounds=bounds, freq=freq)
 
         # -- Process Ocean Model Data -- #
-        mdl_data = _preprocess_model_data(var=self._data[var_name],
-                                          freq=freq,
-                                          timeslice=slice(obs_data.attrs['start_time'], obs_data.attrs['end_time'])
-                                          )
+        if bounds is not None:
+            mdl_data = _subset_data(data=self._data[var_name], bounds=bounds, is_obs=False)
+        mdl_data = _compute_climatology(data=mdl_data, freq=freq)
 
         # -- Regrid Data -- #
         if regrid_to == 'model':
@@ -243,7 +238,7 @@ class ModelValidator():
         if regrid_to == 'model':
             self.results[f"{var_name}_error"] = mdl_error
         elif regrid_to == 'obs':
-            self.results[f"{var_name}_error"] = mdl_error.rename({'lon':'lon_obs', 'lat':'lat_obs'})
+            self.results[f"{var_name}_error"] = mdl_error.rename({'lon':f"lon_{obs['name']}", 'lat':f"lat_{obs['name']}"})
 
         # -- Compute Aggregate Statistics -- #
         if agg_stats:
@@ -255,13 +250,14 @@ class ModelValidator():
     def compute_sst_error(self,
                           sst_name : str = 'tos_con',
                           obs_name : str = 'OISSTv2',
+                          bounds : slice | None = None,
                           freq : str = 'total',
                           regrid_to : str = 'model',
                           method : str = 'bilinear',
                           agg_stats : bool = True,
                           ) -> Self:
         """
-        Compute sea surface temperature error between ocean model and observations.
+        Compute sea surface temperature error between ocean model and observations (model - observation).
 
         Parameters
         ----------
@@ -270,8 +266,12 @@ class ModelValidator():
         obs_name : str, default: ``OISSTv2``
             Name of observational dataset.
             Options include ``OISSTv2``, ``CCI`` and ``HadISST``.
+        bounds : slice, default: ``None``
+            Time bounds to sea surface temperature compute climatologies using ocean model
+            outputs and observations. Default is ``None`` meaning the entire ocean model
+            time series is used.
         freq : str, default: ``total``
-            Climatology frequency to compute (model - observation) sea surface temperature error. 
+            Climatology frequency to compute sea surface temperature error. 
             Options include ``total``, ``seasonal``, ``monthly``.
         regrid_to : str, default: ``model``
             Regrid data to either ``model`` or observations (``obs``) target grid.
@@ -285,12 +285,14 @@ class ModelValidator():
         Returns
         -------
         ModelValidator
-            ModelValidator object including variable, {sst_name}_error, stored in ``data``
-            attribute and aggregate statistics stored in ``stats`` attribute.
+            ModelValidator object including (ocean model - observation) sea surface temperature
+            error, stored in the ``results`` attribute and aggregate statistics stored in the
+            ``stats`` attribute.
         """
         # -- Compute SST Error -- #
         self._compute_2D_error(var_name=sst_name,
-                               obs=dict(name=obs_name, var='sst'),
+                               obs=dict(name=obs_name, region=None, var='sst'),
+                               bounds=bounds,
                                freq=freq,
                                regrid_to=regrid_to,
                                method=method,
@@ -303,6 +305,7 @@ class ModelValidator():
     def plot_sst_error(self,
                        sst_name : str = 'tos_con',
                        obs_name : str = 'OISSTv2',
+                       bounds : slice | None = None,
                        freq : str = 'total',
                        regrid_to : str = 'model',
                        method : str = 'bilinear',
@@ -311,7 +314,7 @@ class ModelValidator():
                        cbar_kwargs : dict = dict(orientation='horizontal', shrink=0.8)
                        ) -> None:
         """
-        Plot sea surface temperature error between ocean model and observations.
+        Plot sea surface temperature error between ocean model and observations (model - observation).
 
         Parameters
         ----------
@@ -320,8 +323,12 @@ class ModelValidator():
         obs_name : str, default: ``OISSTv2``
             Name of observational dataset.
             Options include ``OISSTv2``, ``CCI`` and ``HadISST``.
+        bounds : slice, default: ``None``
+            Time bounds to compute sea surface temperature climatologies using ocean model
+            outputs and observations. Default is ``None`` meaning the entire ocean model
+            time series is used.
         freq : str, default: ``total``
-            Climatology frequency to compute (model - observation) sea surface temperature error.
+            Climatology frequency to compute sea surface temperature error.
             Options include ``total``, ``seasonal``, ``monthly``.
         regrid_to : str, default: ``model``
             Regrid data to either ``model`` or observations (``obs``) target grid.
@@ -342,9 +349,10 @@ class ModelValidator():
             Matplotlib axes object displaying (model - observation) sea surface temperature error.
         """
         # -- Compute SST Error -- #
-        if f"{sst_name}_error" not in self._data.variables:
+        if f"{sst_name}_error" not in self._results.variables:
             self._compute_2D_error(var_name=sst_name,
-                                   obs=dict(name=obs_name, var='sst'),
+                                   obs=dict(name=obs_name, region=None, var='sst'),
+                                   bounds=bounds,
                                    freq=freq,
                                    regrid_to=regrid_to,
                                    method=method,
@@ -354,8 +362,50 @@ class ModelValidator():
         # -- Plot SST Error -- #
         ax = _plot_global_2d(lon=self._lon,
                              lat=self._lat,
-                             var=self.results[f"{sst_name}_error"].squeeze(),
+                             var=self._results[f"{sst_name}_error"].squeeze(),
                              plt_kwargs=plt_kwargs,
                              cbar_kwargs=cbar_kwargs
                              )
         return ax
+
+
+    def load_observations(self,
+                          obs_name : str,
+                          var_name : str,
+                          region : str | None = None,
+                          bounds : slice | None = None,
+                          freq : str | None = None,
+                          ) -> Self:
+        """
+        Load ocean observations from cloud object storage.
+
+        Parameters
+        ----------
+        obs_name : str
+            Name of observational dataset to load.
+        var_name : str
+            Name of variable to load from observational dataset.
+        region : str, default: ``None``
+            Region of ocean observations dataset to load. Default is ``None``
+            meaning the entire dataset is loaded.
+        bounds : slice, default: None
+            Time bounds to compute climatology using ocean observations.
+            Default is ``None`` meaning the entire time series is loaded.
+        freq : str, default: ``None``
+            Climatology frequency of the observational dataset.
+            Options include ``None``, ``total``, ``seasonal``, ``monthly``.
+            Default is ``None`` meaning the entire time series is loaded.
+        
+        Returns
+        -------
+        ModelValidator
+            ModelValidator object including observational data stored in ``obs``
+            attribute.
+        """       
+        # -- Load Ocean Observations Dataset -- #
+        obs_data = self._load_obs_data(obs_name=obs_name, var_name=var_name, region=region, bounds=bounds, freq=freq)
+
+        # -- Store Observational Data -- #
+        self._obs[f"{var_name}_{obs_name}"] = obs_data.rename({'lon':f"lon_{obs_name}", 'lat':f"lat_{obs_name}"})
+
+        return self
