ef526a839d3e9d0c21a0c439f0cc556f8eb59fa2
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-03-04 13:33:31 +0000

    Updated _regrid_data() func to interpolate regional & global (periodic) domains.

--- ValidOcean/regridding.py
+++ ValidOcean/regridding.py
@@ -7,10 +7,14 @@ general circulation model outputs & ocean observations.
 Author: Ollie Tooth (oliver.tooth@noc.ac.uk)
 """
 # -- Import required packages -- #
-import xarray as xr
+import numpy as np
 import xesmf as xe
+import xarray as xr
 
-def _regrid_data(source_grid:xr.DataArray, target_grid:xr.DataArray, method:str='bilinear') -> xr.DataArray:
+def _regrid_data(source_grid : xr.DataArray,
+                 target_grid : xr.DataArray,
+                 method : str = 'bilinear',
+                 ) -> xr.DataArray:
     """
     Regrid data stored on a source grid onto a target grid
     using xesmf Regridder with chosen interpolation method.
@@ -23,9 +27,10 @@ def _regrid_data(source_grid:xr.DataArray, target_grid:xr.DataArray, method:str=
     target_grid : xarray.DataArray
         Target grid to regrid data onto. Longitude and latitude
         coordinates must be called ``lon`` and ``lat``, respectively.
-    method : str
+    method : str, default: 'bilinear
         Interpolation method used by xesmf.
-        Options: ``bilinear``, ``conservative``, ``nearest_s2d``, ``nearest_d2s``.
+        Options include ``bilinear``, ``conservative``, ``conservative_normed``,
+        ``nearest_s2d``, ``nearest_d2s``.
 
     Returns
     -------
@@ -41,7 +46,11 @@ def _regrid_data(source_grid:xr.DataArray, target_grid:xr.DataArray, method:str=
         raise TypeError("``method`` must be a string.")
     
     # -- Regrid Data -- #
-    regridder = xe.Regridder(source_grid, target_grid, method=method, periodic=True)
+    # Identify periodic source grids:
+    lon_bounds = (np.floor(source_grid['lon'].min()), np.ceil(source_grid['lon'].max()))
+    periodic = True if (lon_bounds[0] == -180) & (lon_bounds[0] == 180) else False
+
+    regridder = xe.Regridder(source_grid, target_grid, method=method, periodic=periodic)
     regridded_data = regridder(source_grid)
 
     return regridded_data
